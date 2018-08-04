e0688dab72e062143b81220da92327a77c6949fe
eddie94 <myeddie@naver.com>
2018-08-04 22:48:35 +0900

    VER 1.0

--- Dataset/create_dataset.py
+++ Dataset/create_dataset.py
@@ -2,6 +2,7 @@ import urllib.request
 from urllib.parse import urlencode, quote_plus
 import urllib.parse
 import json
+import os
 
 '''
 2 : REH = 습도 (% 단위)
@@ -17,17 +18,17 @@ pm10 pm25 REH RN1 T1H VEC WSD 가 1개의 feature
 air_url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty'
 weather_url = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib'
 
-air_service_key = '서비스키'
-weather_service_key = '서비스키'
+air_service_key = 'v2cJAMB3TyzncFsrAY5OYI9SyVn9d2560krPsgNXhFB%2F6fh15KNa0ayQQVZuyusYvk0RDXVYowK4j9z7Rk%2FRGw%3D%3D'
+weather_service_key = 'v2cJAMB3TyzncFsrAY5OYI9SyVn9d2560krPsgNXhFB%2F6fh15KNa0ayQQVZuyusYvk0RDXVYowK4j9z7Rk%2FRGw%3D%3D'
 
 x_axis = 60
 y_axis = 127
 
 air_key = {
-quote_plus('numOfRows'): '2000',
+quote_plus('numOfRows'): '20',
 quote_plus('pageNo'): '1',
 quote_plus('stationName'):'종로구',
-quote_plus('dataTerm'): '3MONTH',
+quote_plus('dataTerm'): 'DAILY',
 quote_plus('ver'): '1.3'
 }
 
@@ -36,7 +37,7 @@ quote_plus('base_date'):'',
 quote_plus('base_time'):'',
 quote_plus('nx'):x_axis,
 quote_plus('ny'):y_axis,
-quote_plus('numOfRows'):'2000',
+quote_plus('numOfRows'):'20',
 quote_plus('pageNo'):'1',
 quote_plus('_type'):'json'
 }
@@ -44,7 +45,7 @@ quote_plus('_type'):'json'
 air_QueryParam = '?' + 'serviceKey=' + air_service_key + '&' +urlencode(air_key) + '&_returnType=json'
 
 air_json = urllib.request.urlopen(air_url+air_QueryParam).read()
-
+print(air_json)
 with open('air_data.json','wb') as f:
     f.write(air_json)
 
@@ -142,9 +143,9 @@ with open('air_data.json', 'rt', encoding='UTF-8') as air:
         weather_key[quote_plus('base_time')] = base_time
 
         weather_QueryParam = '?' + 'serviceKey=' + weather_service_key + '&' + urlencode(weather_key)
-
+        print(weather_QueryParam)
         weather_json = urllib.request.urlopen(weather_url + weather_QueryParam).read()
-
+        print(weather_json)
         with open('weather_data.json', 'wb') as weather:
             weather.write(weather_json)
 
@@ -156,11 +157,11 @@ with open('air_data.json', 'rt', encoding='UTF-8') as air:
             pm10 = Pm10ToInt(air_data['list'][i]['pm10Value'])
             pm25 = Pm25ToInt(air_data['list'][i]['pm25Value'])
 
-            REH = REHToInt(weather_data['response']['body']['items']['item'][2]['category'])
-            RN1 = RN1ToInt(weather_data['response']['body']['items']['item'][3]['category'])
-            T1H = T1HToInt(weather_data['response']['body']['items']['item'][5]['category'])
-            VEC = VECToInt(weather_data['response']['body']['items']['item'][7]['category'])
-            WSD = WSDToInt(weather_data['response']['body']['items']['item'][9]['category'])
+            REH = REHToInt(weather_data['response']['body']['items']['item'][2]['obsrValue'])
+            RN1 = RN1ToInt(weather_data['response']['body']['items']['item'][3]['obsrValue'])
+            T1H = T1HToInt(weather_data['response']['body']['items']['item'][5]['obsrValue'])
+            VEC = VECToInt(weather_data['response']['body']['items']['item'][7]['obsrValue'])
+            WSD = WSDToInt(weather_data['response']['body']['items']['item'][9]['obsrValue'])
 
             col.append(pm10)
             col.append(pm25)
@@ -192,4 +193,15 @@ with open('air_data.json', 'rt', encoding='UTF-8') as air:
         except WSDError as e:
             print(e)
 
-    print(len(feature))
+if os.path.isfile("dataset.txt"):
+    with open("dataset.txt", 'a') as f:
+        for row in feature:
+            for col in row:
+                f.write(str(col) + ' ')
+            f.write("\n")
+else:
+    with open("dataset.txt", 'w') as f:
+        for row in feature:
+            for col in row:
+                f.write(str(col) + ' ')
+            f.write("\n")
