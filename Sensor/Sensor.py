dc34f258b5d38ae518f9054d31841354d6363788
= <myeddie@naver.com>
2018-08-06 20:27:18 +0900

    code changed

--- Sensor/Sensor.py
+++ Sensor/Sensor.py
@@ -2,5 +2,7 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=5)
 
-while ser:
-    print(ser)
+while ser.readline():
+    dustValue = ser.readline().decode('utf-8')
+
+
